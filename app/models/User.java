e930c2504b28e8378d6e2a160fc440284032993c
Antoine Mulet <antoine.mulet@ensimag.grenoble-inp.fr>
2016-01-19 09:11:36 +0100

    création user

---
+++ app/models/User.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+ 
+@Entity
+public class User extends Model {
+ 
+    public String email;
+    public String login;
+    public String password;
+    public boolean isAdmin;
+    
+    public User(String email, String login, String password) {
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+ 
+}
