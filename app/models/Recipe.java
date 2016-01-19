b9264e978901d9065845e19719667ad1ff6e18d3
John-Elie Margot <john-elie.margot@ensimag.grenoble-inp.fr>
2016-01-19 09:55:51 +0100

    classe Recipe testée

--- app/models/Recipe.java
+++ app/models/Recipe.java
@@ -12,10 +12,13 @@ public class Recipe extends Model {
 	public String title;
     public Date postedAt;
     public String category;
+    public float rating;
     public int preparationTime;
     public int cookingTime;
     public int numberOfPersons;
-    public List<String> ingredients;
+    
+    @ManyToMany
+    public List<Ingredient> ingredients;
 
     @Lob
     public String content;
@@ -24,16 +27,21 @@ public class Recipe extends Model {
     public User author;
     
     public Recipe(User author, String title, String category, int preparationTime, int cookingTime, int numberOfPersons, 
-    List<String> ingredients, String content) {
+                  List<Ingredient> ingredients, String content) {
         this.author = author;
         this.title = title;
         this.category = category;
+        this.rating = computeRating();
         this.preparationTime = preparationTime;
         this.cookingTime = cookingTime;
         this.numberOfPersons = numberOfPersons;
-        this.ingredients = new ArrayList<String>(ingredients);
+        this.ingredients = new ArrayList<Ingredient>(ingredients);
         this.content = content;
         this.postedAt = new Date();
+    }
+    
+    private float computeRating() {
+        return 0;
     }
     
 }
