422218fe10141edda69b8083806f2d1c7b55de65
John-Elie Margot <john-elie.margot@ensimag.grenoble-inp.fr>
2016-01-25 11:59:52 +0100

    Début gestion authentification

---
+++ app/controllers/Admin.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+ 
+import java.util.*;
+ 
+import models.*;
+
+import play.mvc.Controller;
+
+@With(Secure.class)
+public class Admin extends Controller {
+    
+    @Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byLogin", Security.connected()).first();
+            renderArgs.put("user", user.login);
+        }
+    }
+ 
+    public static void index() {
+        render();
+    }
+}
--- app/controllers/Recipes.java
+++ app/controllers/Recipes.java
@@ -2,7 +2,8 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
- 
+
+@With(Secure.class)
 public class Recipes extends CRUD {
     
 }
