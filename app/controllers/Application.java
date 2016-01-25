b4bee196b6d2383fc7431caa96267dd9ac60b438
Antoine Mulet <antoine.mulet@ensimag.grenoble-inp.fr>
2016-01-25 11:17:51 +0100

    finitions post comment

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,10 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.Play;
+import play.cache.Cache;
 import play.data.validation.*;
+import play.libs.Codec;
+import play.libs.Images;
 
 import java.util.*;
 
@@ -25,19 +28,31 @@ public class Application extends Controller {
     
     public static void show(Long id) {
         Recipe recipe = Recipe.findById(id);
-        render(recipe);
+        String randomID = Codec.UUID();
+        render(recipe, randomID);
     }
 
-    public static void postComment(Long recipeId, String login, @Required String content, int rating) {
+    public static void postComment(Long recipeId, String login, 
+            @Required(message="A message is required") String content, int rating, 
+            @Required(message="Please type the code") String code, String randomID) {
         Recipe recipe = Recipe.findById(recipeId);
+        validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
         if (validation.hasErrors()) {
-            render("Application/show.html", recipe);
+            render("Application/show.html", recipe, randomID);
         }
         // TODO: vérifier que l'utilisateur est connecté pour poster un commentaire
         User user = User.find("byLogin", login).first();
         recipe.addComment(user, content, rating);
         flash.success("Thanks for posting!");
+        Cache.delete(randomID);
         show(recipeId);
+    }
+    
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
     }
 
 }
