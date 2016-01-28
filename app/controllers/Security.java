915afa299f75db751bf99aa190d8892d7bbab08a
Antoine Mulet <antoine.mulet@ensimag.grenoble-inp.fr>
2016-01-28 12:41:07 +0100

    Formulaire de création de compte

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,11 +1,12 @@
 package controllers;
 
 import models.*;
+import play.libs.Crypto;
 
 public class Security extends controllers.Secure.Security {
 
     static boolean authenticate(String username, String password) {
-        return User.connect(username, password) != null;
+        return User.connect(username, Crypto.passwordHash(password)) != null;
     }
     
     static void onDisconnected() {
