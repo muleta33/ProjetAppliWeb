fc82e91fd33bc89880cd44cd66cb6679faba9d52
Antoine Mulet <antoine.mulet@ensimag.grenoble-inp.fr>
2016-01-28 11:26:58 +0100

    Gestion utilisateur connecté pour poster un commentaire

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -23,7 +23,8 @@ public class Application extends Controller {
     public static void index() {
         Recipe frontRecipe = Recipe.find("order by postedAt desc").first();
         List<Recipe> olderRecipes = Recipe.find("order by postedAt desc").from(1).fetch(10);
-        render(frontRecipe, olderRecipes);
+        User user = User.find("byLogin", Security.connected()).first();
+        render(frontRecipe, olderRecipes, user);
     }
     
     public static void show(Long id) {
@@ -37,11 +38,12 @@ public class Application extends Controller {
             @Required(message="Please type the code") String code, String randomID) {
         Recipe recipe = Recipe.findById(recipeId);
         validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
+        // Retrieve and check if the user is connected user
+        User user = User.find("byLogin", Security.connected()).first();
+        validation.required(user).message("You have to be connected to post a comment");
         if (validation.hasErrors()) {
             render("Application/show.html", recipe, randomID);
         }
-        // TODO: vérifier que l'utilisateur est connecté pour poster un commentaire
-        User user = User.find("byLogin", login).first();
         recipe.addComment(user, content, rating);
         flash.success("Thanks for posting!");
         Cache.delete(randomID);
